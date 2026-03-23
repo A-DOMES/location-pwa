869a86acf0db261ea1af70463feefdbbaeb9efc1
A-DOMES <mailsock9@gmail.com>
2026-03-24 03:47:06 +0900

    Update searchModule.js

--- searchModule.js
+++ searchModule.js
@@ -1,12 +1,14 @@
 // searchModule.js
 import { showSingleUser } from './panelModule.js';
+import { globalData } from './updateCycle.js';
 
-function searchUser(userId, globalData) {
+function searchUser(userId) {
   const row = globalData.find(r => r[1] === userId);
   if (row) {
-    showSingleUser(userId, globalData);
+    showSingleUser(userId);
   } else {
-    document.getElementById("user-content").innerHTML = "<p>해당 사용자를 찾을 수 없습니다.</p>";
+    document.getElementById("user-content").innerHTML =
+      "<p>해당 사용자를 찾을 수 없습니다.</p>";
   }
 }
 
