2dc1cb2a2e9fcc2f00e9ecb10f901a076f240ed2
A-DOMES <mailsock9@gmail.com>
2026-03-31 03:03:10 +0900

    Update pwaModule.js

--- pwaModule.js
+++ pwaModule.js
@@ -5,7 +5,7 @@ let previousCoords = {}; // 이전 상태 저장용
 
 /* ---------------------- 데이터 로딩 ---------------------- */
 async function loadData() {
-  const response = await fetch("https://script.google.com/macros/s/AKfycbykQI_iMFUnA9BDsIxktRp_JdMaNTq7Eg6F6UtVjkRLevMW7kRL-eD5x_GWO7j043ilAQ/exec");
+  const response = await fetch("https://script.google.com/macros/s/AKfycbwcQO4NSZtGEnBvQtHuLwpIIT2uBlB70F2_0j1bTSxZGNx23ysGHxiDHlycTDEtBipC3w/exec");
   allData = await response.json();
   showMap();
 }
