163d5a257b2cf5682426121ec4639297af4f8dc2
A-DOMES <mailsock9@gmail.com>
2026-04-02 01:09:33 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,9 @@
-
+// config.js
+const CONFIG = {
+  mapIds: {
+    javascript: "5bd2daaa37ebfc3f6b7ad5e2",
+    android: "5bd2daaa37ebfc3fe0839614",
+    ios: "5bd2daaa37ebfc3faedae42d",
+    fixed: "5bd2daaa37ebfc3f466d5e4c"
+  }
+};
