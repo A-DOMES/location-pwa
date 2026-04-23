976d29e29aafdd35e60a24f2662be5903a8ff5ff
A-DOMES <mailsock9@gmail.com>
2026-04-23 22:37:46 +0900

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const CONFIG = {
   USER_URL: "https://script.google.com/macros/s/AKfycbymXVf0y_ZOWAuyC_xmCsdKVbG2fvJ6Mn4Q-dh-KMHW0X_60tdlvmDOZXxCUwSb8gPJGg/exec",
 
   // ✅ 관리자 서버 URL
-  ADMIN_URL: "https://script.google.com/macros/s/AKfycbxGxQvLJYhyAjhqSGgyQS02LAZyp3CCEeAOHR60srhvH_5GWEvRlgG8xNZJgjstzSW0RQ/exec",
+  ADMIN_URL: "https://script.google.com/macros/s/AKfycbw4Z5E3Fs7-GReBChffEzJw_ncDtGCd_WNII7k4R32VMsxIukVXB_RS6nuWoAQXDhEEXw/exec",
 
   // ✅ client.html (조회용 페이지)
   CLIENT_URL: "client.html",
